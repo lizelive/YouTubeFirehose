894626937fe8ea0cf258adaf75bf9262d0d82502
LizeLive <github@lize.live>
2020-01-08 08:54:06 -0800

    Add project files.

---
+++ YouTubeFirehose/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace YouTubeFirehose
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YouTube Data API: Search");
+            Console.WriteLine("========================");
+
+            try
+            {
+                new Program().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+
+        string pageToken = null;//"CDIQAA";
+
+        private async Task Run()
+        {
+
+
+
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyAoVHFq8E66s8LYgPdye6iNBaBa5Nez8ic",
+                ApplicationName = this.GetType().ToString()
+            });
+
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = ""; // Replace with your search term.
+            searchListRequest.MaxResults = 50;
+
+
+
+            var targetTimeWidth = TimeSpan.FromSeconds(60);
+
+
+            var now = DateTime.Parse("2020-01-08T04:38Z");
+            now = DateTime.Now;
+            var targetTimeStart =  now - TimeSpan.FromDays(1);
+            var targetTimeEnd = targetTimeStart + targetTimeWidth;
+
+
+
+            
+            searchListRequest.Order = SearchResource.ListRequest.OrderEnum.Date;
+            searchListRequest.RelevanceLanguage = "en";
+            searchListRequest.RegionCode = "us";
+
+            searchListRequest.PageToken = pageToken;
+            //specifying this results in livestreams not videos
+            if (false)
+            {
+                searchListRequest.Type = "video";
+                searchListRequest.EventType = SearchResource.ListRequest.EventTypeEnum.Completed;
+            }
+
+            searchListRequest.PublishedAfter = targetTimeStart;
+            searchListRequest.PublishedBefore = targetTimeEnd;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<string> videos = new List<string>();
+            List<string> channels = new List<string>();
+            List<string> playlists = new List<string>();
+
+
+            Console.WriteLine($"found {searchListResponse.PageInfo.TotalResults} videos but retrived {searchListResponse.Items.Count} in {targetTimeWidth} starting at targetTimeStart with {searchListResponse.NextPageToken} token");
+
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                if(searchResult.Id.Kind== "youtube#video")
+                {
+
+                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                }
+            }
+
+            Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+            Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+            Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
+        }
+    }
+}
---
+++ YouTubeFirehose/Video.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouTubeFirehose
+{
+    class Video
+    {
+        /// <summary>
+        /// The value that YouTube uses to uniquely identify the channel that published the resource that the search result identifies.
+        /// </summary>
+        public string ChannelId { get; set; }
+
+        /// <summary>
+        /// A description of the search result.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// It indicates if the resource (video or channel) has upcoming/active live broadcast
+        //  content. Or it's "none" if there is not any upcoming/active live broadcasts.
+        /// </summary>
+        public string LiveBroadcastContent { get; set; }
+
+        /// <summary>
+        /// System.DateTime representation of Google.Apis.YouTube.v3.Data.SearchResultSnippet.PublishedAtRaw.
+        /// </summary>
+        public DateTime PublishedAt { get; set; }
+        //
+        // Summary:
+        //     A map of thumbnail images associated with the search result. For each object
+        //     in the map, the key is the name of the thumbnail image, and the value is an object
+        //     that contains other information about the thumbnail.
+        public string ThumbnailUrl { get; set; }
+
+
+        /// <summary>
+        /// The title of the search result.
+        /// </summary>
+        public string Title { get; set; }
+        //
+        // Summary:
+        //     The ETag of the item.
+        public string ETag { get; set; }
+    }
+}
